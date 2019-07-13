83651d982ed78c8cb111b7cd41dcabd010de3659
Ben Quigley <ben.quigley@protonmail.com>
2019-07-13 11:44:37 -0400

    models

--- checklists/models.py
+++ checklists/models.py
@@ -1,3 +1,9 @@
+"""Checklist-related models."""
 from django.db import models
 
+
 # Create your models here.
+
+class Item(models.Model):
+    """An item on a checklist."""
+    item_text = models.CharField(max_length=200)
